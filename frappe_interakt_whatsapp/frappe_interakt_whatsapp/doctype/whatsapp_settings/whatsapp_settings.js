fe43d854c58ed96aebaa3d6c078229de2d65a740
Mangesh Redij <mangesh@vmi1455331.contaboserver.net>
2023-09-28 12:35:00 +0530

    Initial commit

---
+++ frappe_interakt_whatsapp/frappe_interakt_whatsapp/doctype/whatsapp_settings/whatsapp_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Inventeam Solutions Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Whatsapp Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
