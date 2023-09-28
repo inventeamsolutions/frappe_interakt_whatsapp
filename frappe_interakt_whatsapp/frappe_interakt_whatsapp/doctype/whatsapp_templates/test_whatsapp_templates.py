aefb98a5b86a70540396b3ee48749162a76f8c48
Mangesh Redij <mangesh@vmi1455331.contaboserver.net>
2023-09-28 20:18:35 +0530

    28092023_2018

---
+++ frappe_interakt_whatsapp/frappe_interakt_whatsapp/doctype/whatsapp_templates/test_whatsapp_templates.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Inventeam Solutions Pvt. Ltd. and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestWhatsappTemplates(FrappeTestCase):
+	pass
---
+++ frappe_interakt_whatsapp/frappe_interakt_whatsapp/doctype/whatsapp_templates/whatsapp_templates.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Inventeam Solutions Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class WhatsappTemplates(Document):
+	pass
